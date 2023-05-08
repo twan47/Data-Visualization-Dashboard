411b0318e98850cbb3f0c7419b81110a9aa0efe0
Twan47 <anthonynewell171@gmail.com>
2023-05-08 12:07:43 -0400

    import bug fixes

--- data-dash.py
+++ data-dash.py
@@ -3,8 +3,8 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 
 #read dataset notes
